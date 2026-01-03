60323e0e5c84232bd5fced5864fb5a726b4e4d30
Shivani Gupta <166619882+ShivaniGupta164@users.noreply.github.com>
2026-01-03 16:48:13 +0530

    Modify JWT cookie settings for security
    Updated cookie settings for JWT to support HTTPS and Render.

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -12,11 +12,12 @@ export const generateToken = (userId, res) => {
   });
 
   res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000, // MS
-    httpOnly: true, // prevent XSS attacks: cross-site scripting
-    sameSite: "strict", // CSRF attacks
-    secure: ENV.NODE_ENV === "development" ? false : true,
-  });
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+  httpOnly: true,
+  sameSite: "none",   // 🔥 MUST for Render
+  secure: true,       // 🔥 MUST for HTTPS
+});
+
 
   return token;
 };
